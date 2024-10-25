05d946c13afd7ad02dfe4998cd20774e9488b192
kromenz <andrerafael892@gmail.com>
2024-10-25 12:36:17 +0100

    cleaned the repository

--- settings.py
+++ settings.py
@@ -1,10 +1,16 @@
 import json
 
-def string_conexao():
-#Conexao DB atraves do ficheiro Json   
+def conexao_capture():
     fileObject = open("settings/settings.json", "r")
     jsonContent = fileObject.read()
     aList = json.loads(jsonContent)
     #resultado da pesquisa do Json
-    connection_string=aList[0]['connection_string_db_lms']
+    connection_string=aList[0]['connection_string_capture']
+    return connection_string
+
+def conexao_mms():
+    fileObject = open("settings/settings.json", "r")
+    jsonContent = fileObject.read()
+    aList = json.loads(jsonContent)
+    connection_string=aList[1]['connection_string_mms']
     return connection_string
